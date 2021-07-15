f893dfd96a51c6d6fe63ba4764019b7bb2d11f67
Giuseppe Rota <rota.giuseppe@gmail.com>
2021-07-15 15:40:08 +0200

    refs sparkfabrik-innovation-team/board#591: refactor exit codes (#42)

--- templates/scripts/ci_releases/drupal/update_drupal_modules_metadata_from_branchname.sh
+++ templates/scripts/ci_releases/drupal/update_drupal_modules_metadata_from_branchname.sh
@@ -22,36 +22,38 @@
 #     - if: ($CI_PIPELINE_SOURCE == "merge_request_event") && ($CI_MERGE_REQUEST_SOURCE_BRANCH_NAME =~ /^release\/.*/) && ($CI_MERGE_REQUEST_TARGET_BRANCH_NAME == $CI_DEFAULT_BRANCH)
 #   script:
 #     - SCRIPT=update_drupal_modules_metadata_from_branchname.sh
-#     - curl -Ls -o ./.gitlab/scripts/$SCRIPT
+#     - SCRIPT_DOWNLOAD_PATH=/tmp
+#     - curl -Ls -o "$SCRIPT_DOWNLOAD_PATH/$SCRIPT"
 #       https://github.com/sparkfabrik/spark-k8s-deployer/raw/master/templates/scripts/ci_releases/drupal/$SCRIPT
-#       && chmod +x ./.gitlab/scripts/$SCRIPT
-#       && ./.gitlab/scripts/$SCRIPT
+#       && chmod +x "$SCRIPT_DOWNLOAD_PATH/$SCRIPT"
+#       && "$SCRIPT_DOWNLOAD_PATH/$SCRIPT"
 
 set -o errtrace
 set -o errexit
 set -o pipefail
 
+SCRIPT_DOWNLOAD_PATH=/tmp
 declare -a SCRIPTS=("validate_semver.sh" "setup_repo_for_writing.sh")
 for SCRIPT in "${SCRIPTS[@]}"; do
-  curl -Ls -o ./.gitlab/scripts/"$SCRIPT" \
+  curl -Ls -o "$SCRIPT_DOWNLOAD_PATH/$SCRIPT" \
   https://github.com/sparkfabrik/spark-k8s-deployer/raw/master/templates/scripts/ci_releases/"$SCRIPT" \
-  && chmod +x ./.gitlab/scripts/"$SCRIPT"
+  && chmod +x "$SCRIPT_DOWNLOAD_PATH/$SCRIPT"
 done
 SCRIPT_DRUPAL="update_drupal_info_yml_metadata.sh"
-curl -Ls -o ./.gitlab/scripts/"$SCRIPT_DRUPAL" \
+curl -Ls -o "$SCRIPT_DOWNLOAD_PATH/$SCRIPT_DRUPAL" \
   https://github.com/sparkfabrik/spark-k8s-deployer/raw/master/templates/scripts/ci_releases/drupal/"$SCRIPT_DRUPAL" \
-  && chmod +x ./.gitlab/scripts/"$SCRIPT_DRUPAL"
+  && chmod +x "$SCRIPT_DOWNLOAD_PATH/$SCRIPT_DRUPAL"
 
 # Extract version from the branch name, check that is a valid semver string,
 # and that is greater (semver-wise) to the latest tag in the repo.
 VERSION_FROM_BRANCH_NAME=${CI_COMMIT_REF_NAME##release/}
 echo VERSION_FROM_BRANCH_NAME="$VERSION_FROM_BRANCH_NAME"
-./.gitlab/scripts/validate_semver.sh "$VERSION_FROM_BRANCH_NAME"
+"$SCRIPT_DOWNLOAD_PATH"/validate_semver.sh "$VERSION_FROM_BRANCH_NAME"
 
 # Requires GITLAB_PROJECT_RW_AND_API_TOKEN vars.
-./.gitlab/scripts/setup_repo_for_writing.sh
+"$SCRIPT_DOWNLOAD_PATH"/setup_repo_for_writing.sh
 
-./.gitlab/scripts/update_drupal_info_yml_metadata.sh "$DRUPAL_PROJECT_NAME" "$VERSION_FROM_BRANCH_NAME"
+"$SCRIPT_DOWNLOAD_PATH"/update_drupal_info_yml_metadata.sh "$DRUPAL_PROJECT_NAME" "$VERSION_FROM_BRANCH_NAME"
 
 DESTINATION_BRANCH="${CI_MERGE_REQUEST_SOURCE_BRANCH_NAME:-${CI_COMMIT_BRANCH:-$CI_COMMIT_REF_NAME}}"
 git add .
