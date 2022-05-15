1bf1b5a71c25c995addf3af79d520197f6d93cea
Daniele Monti <62102073+Monska85@users.noreply.github.com>
2022-05-15 23:34:49 +0200

    refs #000: check directory before perform executable check (#81)

--- templates/scripts/ci_releases/validate_hooks.sh
+++ templates/scripts/ci_releases/validate_hooks.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-if find .hooks/ -type f -name '*.sh' ! -executable | grep -q .; then
-  echo The following hook files are not executable, please fix.
-  find .hooks/ -type f -name '*.sh' ! -executable
-  exit 1
+if [ -d .hooks ]; then
+  if find .hooks/ -type f -name '*.sh' ! -executable | grep -q .; then
+    echo The following hook files are not executable, please fix.
+    find .hooks/ -type f -name '*.sh' ! -executable
+    exit 1
+  fi
 fi
